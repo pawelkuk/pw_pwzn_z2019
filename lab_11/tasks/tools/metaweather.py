e34e62d4d622f707885cdecaef5efc18664e6c6e
Paweł Kukliński <paffcio314@gmail.com>
2020-01-16 16:12:58 +0100

    Add black.

--- lab_11/tasks/tools/metaweather.py
+++ lab_11/tasks/tools/metaweather.py
@@ -3,19 +3,14 @@ from urllib.parse import urljoin
 import requests
 
 
-API_URL = 'https://www.metaweather.com/api/'
+API_URL = "https://www.metaweather.com/api/"
 
 
-def get_metaweather(url, query=None, timeout=5.):
-    get_params = dict(
-        timeout=timeout,
-    )
+def get_metaweather(url, query=None, timeout=5.0):
+    get_params = dict(timeout=timeout,)
     if query:
-        get_params['params'] = query
-    response = requests.get(
-        url,
-        **get_params
-    )
+        get_params["params"] = query
+    response = requests.get(url, **get_params)
     response.raise_for_status()
     try:
         return response.json()
@@ -23,23 +18,20 @@ def get_metaweather(url, query=None, timeout=5.):
         raise RuntimeError from exc
 
 
-def get_cities_woeid(query, timeout=5.):
-    location_url = urljoin(API_URL, 'location/search')
+def get_cities_woeid(query, timeout=5.0):
+    location_url = urljoin(API_URL, "location/search")
     query = dict(query=query)
     content = get_metaweather(location_url, query, timeout)
-    return {
-        elem['title']: elem['woeid']
-        for elem in content
-    }
+    return {elem["title"]: elem["woeid"] for elem in content}
 
 
-if __name__ == '__main__':
-    assert get_cities_woeid('Warszawa') == {}
-    assert get_cities_woeid('War') == {
-        'Warsaw': 523920,
-        'Newark': 2459269,
+if __name__ == "__main__":
+    assert get_cities_woeid("Warszawa") == {}
+    assert get_cities_woeid("War") == {
+        "Warsaw": 523920,
+        "Newark": 2459269,
     }
     try:
-        get_cities_woeid('Warszawa', 0.1)
+        get_cities_woeid("Warszawa", 0.1)
     except Exception as exc:
         isinstance(exc, requests.exceptions.Timeout)
