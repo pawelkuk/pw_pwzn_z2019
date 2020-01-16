e358a35518e4e09854769f0145782f01bc2ea646
Paweł Kukliński <paffcio314@gmail.com>
2020-01-16 14:44:24 +0100

    Add task 1 in progress.

---
+++ lab_11/tasks/tests/test_task1.py
@@ -0,0 +1,43 @@
+import pytest
+
+from lab_11.tasks.tools.calculator import (
+    Calculator,
+    CalculatorError,
+    EmptyMemory,
+    NotNumberArgument,
+    WrongOperation,
+)
+
+test_run_data = [
+    ("+", 1, 2, 3),
+    ("-", 1, 2, -1),
+    ("*", 1, -2, -2),
+    ("/", 1, -2, -0.5),
+]
+
+test_run__invalid_data = [
+    ("^", 1, 2, WrongOperation),
+    ("-", "foo", 2, NotNumberArgument),
+    ("*", 1, "bar", NotNumberArgument),
+    ("/", 1, 0, CalculatorError),
+    ("/", 1, 0.0, CalculatorError),
+]
+
+
+@pytest.fixture(scope="function")
+def calculator():
+    print("\nNew calculator...")
+    return Calculator()
+
+
+@pytest.mark.parametrize("operator,arg1,arg2,expected", test_run_data)
+def test_run_valid_operations(operator, arg1, arg2, expected, calculator):
+    result = calculator.run(operator, arg1, arg2)
+    assert result == expected
+
+
+@pytest.mark.parametrize("operator,arg1,arg2,expected", test_run__invalid_data)
+def test_run_invalid_data_raises_exceptions(operator, arg1, arg2, expected, calculator):
+    with pytest.raises(expected):
+        calculator.run(operator, arg1, arg2)
+
--- lab_11/tasks/tests/tests_task1.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from lab_11.tasks.tools.calculator import (
-    Calculator,
-    CalculatorError,
-    EmptyMemory,
-    NotNumberArgument,
-    WrongOperation,
-)
