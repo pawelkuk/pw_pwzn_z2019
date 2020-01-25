76c0f628673dbd30f62784a5ef6776b0f2a2b3fe
Paweł Kukliński <paffcio314@gmail.com>
2020-01-25 21:53:58 +0100

    Add task 2.

--- lab_12/tasks/task_2.py
+++ lab_12/tasks/task_2.py
@@ -4,7 +4,15 @@ from time import time
 
 
 def log_run(fun):
-    pass
+    def wrapper(*args, **kwargs):
+        start = time()
+        print(f"{datetime.now()}| function{ fun.__name__ } called with:")
+        print(f"{len(args)} positional parameters")
+        print(f'optional parameters: {kwargs.keys() if len(kwargs) else "-"}')
+        res = fun(*args, **kwargs)
+        print(f"returned: {res} ({time() - start:+.2e})")
+
+    return wrapper
 
 
 @log_run
@@ -12,16 +20,16 @@ def fun(*args, **kwargs):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     decorated_sum = log_run(sum)
-    decorated_sum([1,2,3])
-    fun(1, 2, 'a', bb=1)
+    decorated_sum([1, 2, 3])
+    fun(1, 2, "a", bb=1)
     # Przykładowy log
     # 2020-01-23T21:09:55| function sum called with:
-    # 1 postional parameters
+    # 1 positional parameters
     # optional parameters: -
     # returned: 6 (1.43e-06s)
     # 2020-01-23T21:09:55| function fun called with:
-    # 3 postional parameters
+    # 3 positional parameters
     # optional parameters: bb
     # returned: None (1.43e-06s)
